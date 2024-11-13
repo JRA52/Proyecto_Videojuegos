11c8349e6311b756f9f248156e83b8232d998191
Gabriel Perez <logabriel35@gmail.com>
2024-11-12 23:58:10 -0400

    first commit

---
+++ src/definitions/creatures.py
@@ -0,0 +1,44 @@
+"""
+ISPPJ1 2024
+Study Case: Super Martian (Platformer)
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the definition for creatures.
+"""
+
+from typing import Dict, Any
+
+from src.states.entities import creatures_states
+
+CREATURES: Dict[int, Dict[str, Any]] = {
+    48: {
+        "texture_id": "creatures",
+        "walk_speed": 10,
+        "animation_defs": {"walk": {"frames": [48, 49], "interval": 0.25}},
+        "states": {"walk": creatures_states.SnailWalkState},
+        "first_state": "walk",
+    },
+    52: {
+        "texture_id": "creatures",
+        "walk_speed": 15,
+        "animation_defs": {"walk": {"frames": [52, 53], "interval": 0.18}},
+        "states": {"walk": creatures_states.SnailWalkState},
+        "first_state": "walk",
+    },
+    11: {
+        "texture_id": "creatures",
+        "walk_speed": 18,
+        "animation_defs": {"walk": {"frames": [11, 11], "interval": 0.18}},
+        "states": {"walk": creatures_states.SnailWalkState},
+        "first_state": "walk",
+    },
+    8: {
+        "texture_id": "creatures",
+        "walk_speed": 10,
+        "animation_defs": {"walk": {"frames": [8, 9], "interval": 0.18}},
+        "states": {"walk": creatures_states.SnailWalkState},
+        "first_state": "walk",
+    },
+}
